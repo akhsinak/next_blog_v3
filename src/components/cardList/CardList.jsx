acb38cbb5d704c78d263a16d077eb7812f7b7582
kanishka <pstkanishka@gmail.com>
2023-09-15 18:14:25 +0530

    fixed bad import in cardlist

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -3,7 +3,7 @@ import styles from "./cardList.module.css";
 import Pagination from "../pagination/Pagination";
 import Image from "next/image";
 import Card from "../card/Card";
-import LoadingCard from "../card/loadingCard";
+import LoadingCard from "../card/LoadingCard";
 
 
 // console.log("zindagi card")
@@ -45,11 +45,6 @@ const CardList = async ({ page, cat }) => {
               <LoadingCard />
             </>
           )
-
-          // <>
-          //   <LoadingCard />
-          //   <LoadingCard />
-          // </>
 
         }
       </div>
