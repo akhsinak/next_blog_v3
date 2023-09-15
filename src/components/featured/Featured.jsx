b42bac17c5da6e65ca06b28b21597a6ff243f6bb
kanishka <pstkanishka@gmail.com>
2023-09-15 18:57:48 +0530

    fixed dark mode bug in featured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -26,7 +26,7 @@ const Featured = () => {
   useEffect(() => {
     async function fetchData() {
       try {
-        const data = await getData(1, "travel");
+        const data = await getData(1, "style");
         if (data.posts && data.posts.length > 0) {
           const n = data.posts.length
           const randomIndex = Math.floor(Math.random() * n); // Generate a random index
@@ -63,19 +63,15 @@ const Featured = () => {
             </div>
             <div className={styles.textContainer}>
               <h1 className={styles.postTitle}>{firstArticle.title}</h1>
-              {/* Display either a truncated or full description based on the state */}
               <div
-                className={styles.postDesc}
                 dangerouslySetInnerHTML={{
                   __html: showFullContent ? firstArticle.desc.substring(0, 700) : firstArticle.desc.substring(0, 400) + "..."
                 }}
+                className={styles.postDesc}
               />
               <button className={styles.button} onClick={handleReadMoreClick}>
                 {showFullContent ? "Read Less" : "Read More"}
               </button>
-              {/* <Link href={`/posts/${firstArticle.slug}`} className={styles.link}>
-              Read More
-            </Link> */}
             </div>
           </>
 
